fbebb5ff59b86d0a0d14654fcbd2de4a9fa8a99e
cristiano ferreira <cristianowakaba@gmail.com>
2024-04-20 21:50:53 -0300

    aula 446

--- BNG/app/controllers/Main.php
+++ BNG/app/controllers/Main.php
@@ -46,17 +46,46 @@ class Main extends BaseController
             $this->index();
             return;
         }
+
         $validation_errors = [];
         if (empty($_POST['text_username']) || empty($_POST['text_password'])) {
             $validation_errors[] = "Username e Password são obrigatórios";
         }
+        $username = $_POST['text_username'];
+        $password = $_POST['text_password'];
+
+        if (!filter_var($username, FILTER_VALIDATE_EMAIL)) {
+            $validation_errors[] = 'O username tem que ser um email válido.';
+        }
+        if (strlen($username) < 5 || strlen($username) > 50) {
+            $validation_errors[] = 'O username tem que ter entre 5 e 50 caracteres.';
+        }
+        if (strlen($password) < 6 || strlen($password) > 12) {
+            $validation_errors[] = 'O password tem que ter entre 6 e 12 caracteres.';
+        }
+
+
+
+
+
         if (!empty($validation_errors)) {
             $_SESSION['validation_errors'] = $validation_errors;
             $this->login_frm();
             return;
-            $username = $_POST['text_username'];
-            $password = $_POST['text_password'];
-            echo $username . '<br>. ' . $password;
+        }
+        $model = new Agents();
+        $result = $model->check_login($username, $password);
+        if ($result['status']) {
+            echo 'ok!';
+        } else {
+            echo 'nok!';
         }
     }
 }
+/* 
+admin@bng.com   Aa123456
+admin1@bng.com   Aa123456
+admin2@bng.com   Aa123456
+aula 445
+
+*/
--- BNG/app/models/Agents.php
+++ BNG/app/models/Agents.php
@@ -6,5 +6,27 @@ use bng\Models\BaseModel;
 
 class Agents extends BaseModel
 {
- 
+    //=============================================================
+    public function check_login($username, $password)
+{
+    $params = [
+        ':username' => $username
+    ];
+    $this->db_connect();
+    $results = $this->query(
+        "SELECT id, passwrd FROM agents"." WHERE AES_ENCRYPT(:username,'".MYSQL_AES_KEY."') = name",$params);
+   if($results->affected_rows==0){
+    return [
+        'status'=>false,
+    ];
+   }
+   if(!password_verify($password,$results->results[0]->passwrd)){
+    return[
+        'status'=>false,
+    ];
+   }
+   return[
+    'status'=>true,
+   ];
+}
 }
